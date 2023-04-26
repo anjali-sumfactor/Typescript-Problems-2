f206ab56b55f106ba352616d2c994ecde9726ac1
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-26 12:20:59 +0530

    validate IP address

---
+++ 10th-Problem/app.ts
@@ -0,0 +1,9 @@
+//10. Write a function that takes a string as input and returns true if it is a valid IPv4 address in dot-decimal notation, and false otherwise.
+
+export function validateIPaddress(ipaddress: string): boolean {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+        return true;
+    }
+    return false;
+}
+console.log(validateIPaddress("01.102.103104"));
