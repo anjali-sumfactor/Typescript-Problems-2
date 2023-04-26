d0543f424de530d5d2c00758be3026144007e51d
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-04-26 17:33:18 +0530

    longest increasing subsequence

---
+++ 2nd-Problem/app.ts
@@ -0,0 +1,18 @@
+//2. Write a function that takes an array of integers as input and returns the length of the longest increasing subsequence.
+
+export function longestIncresingSubsequence(array: number[]): number {
+    if (array.length == 0) return 0;
+
+    let arr = Array(array.length).fill(1);
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (array[i] > array[j]) {
+                arr[i] = Math.max(arr[i], arr[j] + 1);
+            }
+        }
+    }
+    return Math.max(...arr);
+
+}
+console.log(longestIncresingSubsequence([100, 20, 9, 35, 45, 29, 101]));
